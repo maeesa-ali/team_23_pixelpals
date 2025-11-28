3417ac1fd3f5076fe97c8bc064644c59aed0dd07
maeesa-ali <210091232@aston.ac.uk>
2025-11-28 12:43:53 +0000

    Update login.php

--- Term 1B/MVP/backend/login.php
+++ Term 1B/MVP/backend/login.php
@@ -1,1 +1,8 @@
+// login.php will process login form submissions in the future, more code will be put in later stages.
 <?php 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   
+}
+ // meaning of the code above: did the user press submit on a form that sends data?
+    // purpose of doing the code is so systems tester can test the login form
