297bb16768ef32e750c7bd3878cf6f7aaa4b565d
maeesa-ali <210091232@aston.ac.uk>
2026-03-15 21:35:21 +0000

    Update logout.php

--- Team23_PixelPals_Term2_Final/public/logout.php
+++ Team23_PixelPals_Term2_Final/public/logout.php
@@ -1,1 +1,14 @@
+<?php
 
+// Start session so we can clear it
+session_start();
+
+// Remove all session data
+session_unset();
+
+// Destroy session completely
+session_destroy();
+
+// Redirect user back to login page
+header('Location: /Team23_PixelPals_Term2_Final/public/login.php');
+exit;
