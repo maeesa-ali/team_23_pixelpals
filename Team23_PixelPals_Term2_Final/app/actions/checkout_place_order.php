3a68f11528404e383d802436c1de49dfe2d883f7
gb-joel <240119397@aston.ac.uk>
2026-03-18 21:46:21 +0000

    completed checkout_place_order.php

--- Team23_PixelPals_Term2_Final/app/actions/checkout_place_order.php
+++ Team23_PixelPals_Term2_Final/app/actions/checkout_place_order.php
@@ -28,4 +28,28 @@ if (count($checkout_items) === 0){
     header('Location: basket.php');
     exit;
 }
+
+$order_sql = "INSERT INTO orders (UserID, Status) VALUES (?, 'Processing')";
+$order_stmt = $db->prepare($order_sql);
+$order_stmt->execute([$UserID]);
+$OrderID = $db->lastInsertId();
+
+$item_sql = "INSERT INTO orderitem (OrderID, ProductID, Quantity, totalProductPrice, Subtotal) VALUES (?, ?, ?, ?, ?)";
+$item_stmt = $db->prepare($item_sql);
+ 
+$stock_sql = "UPDATE product SET Stock = Stock - ? WHERE ProductID = ?";
+$stock_stmt = $db->prepare($stock_sql);
+
+foreach ($checkout_items as $item){
+    $Subtotal = $item['totalProductPrice'] * $item['Quantity'];
+    $item_stmt->execute([$OrderID, $item['ProductID'], $item['Quantity'], $item['totalProductPrice'], $Subtotal]);
+    $stock_stmt->execute([$item['Quantity'], $item['ProductID']]);
+}
+
+$clear_sql = "DELETE FROM basketitem WHERE BasketID = ?";
+$clear_stmt = $db->prepare($clear_sql);
+$clear_stmt->execute([$BasketID]);
+
+header("Location: ../../public/order_success.php?order_id=" . $OrderID);
+exit;
 ?>
