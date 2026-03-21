77a6fe14dfb05b865cf2b7ad4f5899b5dc1057d2
gb-joel <240119397@aston.ac.uk>
2026-03-21 22:50:15 +0000

    finished php side

--- Team23_PixelPals_Term2_Final/public/admin/orders.php
+++ Team23_PixelPals_Term2_Final/public/admin/orders.php
@@ -1,1 +1,23 @@
+<?php
+session_start();
+require '../config/db.php';
 
+if (!isset($_SESSION['UserID']) || $_SESSION['Role'] !== 'Admin') {
+    header('Location: ../../public/login.php');
+    exit;
+}
+
+$sql = "SELECT o.OrderID, o.Status, u.Username, u.Email FROM orders o JOIN users u ON o.UserID = u.UserID ORDER BY CASE WHEN o.Status = 'Processing' THEN 1 ELSE 2 END, o.OrderID DESC";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard - Orders</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+<body>
