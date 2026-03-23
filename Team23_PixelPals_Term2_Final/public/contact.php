8f45aa015906ef31e79bb8d7c528b98b612125ec
dia kaur <230219232@aston.ac.uk>
2026-03-23 07:06:36 +0000

    Update contact.php

--- Team23_PixelPals_Term2_Final/public/contact.php
+++ Team23_PixelPals_Term2_Final/public/contact.php
@@ -108,29 +108,29 @@
       <nav class="navbar">
 
         <div class="nav-left">
-          <a href="/index.php">
+          <a href="index.php">
             <img src="/assets/img/logo.png" class="logo" alt="PixelPals Logo">
           </a>
 
-          <a href="/index.php">PixelPals</a>
+          <a href="index.php">PixelPals</a>
         </div>
 
         <div class="nav-links">
-          <a href="/index.php">Home</a>
-          <a href="/products.php">Products</a>
-          <a href="/about.php">About</a>
-          <a href="/contact.php">Contact</a>
+          <a href="index.php">Home</a>
+          <a href="products.php">Products</a>
+          <a href="about.php">About</a>
+          <a href="contact.php">Contact</a>
         </div>
 
         <div class="nav-right">
-          <a href="/basket.php">Basket</a>
-          <a href="/account.php">Account</a>
+          <a href="basket.php">Basket</a>
+          <a href="account.php">Account</a>
 
           <?php if(isset($_SESSION['user_id'])): ?>
           <a href="/logout.php">Logout</a>
           <?php else: ?>
-          <a href="/login.php">Login</a>
-          <a href="/signup.php">Signup</a>
+          <a href="login.php">Login</a>
+          <a href="signup.php">Signup</a>
           <?php endif; ?>
 
           <?php if(isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
